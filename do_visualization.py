c388afb32ef0132ab314f74f4c3d74708098d86c
Benson N N <112963428+Nyotabenson@users.noreply.github.com>
2024-10-31 16:32:01 +0300

    Update do_visualization.py

--- do_visualization.py
+++ do_visualization.py
@@ -66,7 +66,7 @@ def daily_orders():
         xaxis=dict(
             tickformat='%Y-%m-%d',  
             tickangle=-45,
-            color = 'black'
+            color = '#e52020'
         ),
         title={
             'text': "Daily Order Processing",  
